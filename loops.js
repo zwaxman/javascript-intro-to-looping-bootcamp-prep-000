22d9a8140d91cb13237c0c543ba38b4df3c70ab8
Zachary M Waxman <zwaxman@gmail.com>
2019-07-17 00:34:18 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -10,20 +10,12 @@ function forLoop(arr){
     return arr
   }
 
-// describe('forLoop(array)', () => {
-//     it('adds `"I am ${i} strange loop${i === 0 ? \'\' : \'s\'}."` to an array 25 times', () => {
-//       const [array, t] = makeArray()
-//       const strangeArray = forLoop(array)
-//       const testArray = strangeArray.slice(array.length)
-
-//       let first = "I am 1 strange loop."
-//       let rest = "I am 24 strange loops."
-
-//       expect(strangeArray[11]).to.equal(first)
-//       expect(strangeArray[34]).to.equal(rest)
-//       expect(strangeArray.length).to.equal(t + 25)
-//     })
-//   })
+function whileLoop(n){
+  while(n>=0){
+    console.log(n)
+    n--
+  }
+}
 
 //   describe('whileLoop(n)', () => {
 //     it('counts down from n to 0', () => {
